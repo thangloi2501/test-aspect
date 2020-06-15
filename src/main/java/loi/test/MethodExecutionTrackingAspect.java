27a9aec973ed514e58d3ce227cb368bf9144a57b
thangloi2501 <thangloi2501@gmail.com>
2020-06-15 12:21:23 +0800

    update

--- src/main/java/loi/test/MethodExecutionTrackingAspect.java
+++ src/main/java/loi/test/MethodExecutionTrackingAspect.java
@@ -1,18 +1,3 @@
-/*******************************************************************************
- * Copyright(c) FriarTuck Pte Ltd ("FriarTuck"). All Rights Reserved.
- *
- * This software is the confidential and proprietary information of FriarTuck.
- * ("Confidential Information"). You shall not disclose such Confidential
- * Information and shall use it only in accordance with the terms of the license
- * agreement you entered into with FriarTuck.
- *
- * FriarTuck MAKES NO REPRESENTATIONS OR WARRANTIES ABOUT THE SUITABILITY OF THE
- * SOFTWARE, EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE IMPLIED
- * WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE, OR NON-
- * INFRINGEMENT. FriarTuck SHALL NOT BE LIABLE FOR ANY DAMAGES SUFFERED BY LICENSEE
- * AS A RESULT OF USING, MODIFYING OR DISTRIBUTING THIS SOFTWARE OR ITS DERIVATIVES.
- ******************************************************************************/
-
 package loi.test;
 
 import org.aspectj.lang.ProceedingJoinPoint;
@@ -21,13 +6,6 @@ import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
 
-/**
- * This aspect will record all methods executed in chronological order and provide
- * detailed analytic at the end of a request.
- *
- * @author James Tran
- *
- */
 @Aspect
 @Component
 public class MethodExecutionTrackingAspect {
